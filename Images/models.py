4367b6d410c1822ee4ac6e38e27c3ea4ba847bde
Shubham Garg <48384772+shubhamvgarg@users.noreply.github.com>
2019-04-03 13:29:30 +0530

    Update models.py

--- Images/models.py
+++ Images/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+#table to store image description and title , generated key will be used as foreign key
 class ImageFiles(models.Model):
     Title=models.TextField()
     Description=models.TextField()
     def __str__(self):
 	       return self.Title+" "+self.Description+" "
-
+#stores the url for the uploaded files and foreign key 
 class Image_Attachment(models.Model):
     key = models.ForeignKey(ImageFiles)
     file = models.TextField( null=True, blank=True)
